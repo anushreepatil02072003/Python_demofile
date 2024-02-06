8a644b9079934b22c86fdd1decd4904bde168356
anushreepatil02072003 <123636244+anushreepatil02072003@users.noreply.github.com>
2024-02-06 10:35:27 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
-print("Hello World")
+# function with two arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum: ", sum)
+
+# function call with two values
+add_numbers(5, 4)
